268d4a2ab9a91404cf47e17da3099dc2c1e8795f
samuelhackwill <hackmew@gmail.com>
2026-02-19 19:12:31 +0100

    Add server startup polling and path logging

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,6 +1,11 @@
 import { Meteor } from 'meteor/meteor';
+
 import { ensureMessagesIndexes } from '/imports/api/messages/server/indexes';
+import { getDataDir } from '/imports/server/filePaths';
+import { startMessagesPoller } from '/imports/startup/server/jobs/messagesPoller';
 
 Meteor.startup(async () => {
+  console.info(`[storage] data dir: ${getDataDir()}`);
   await ensureMessagesIndexes();
+  await startMessagesPoller();
 });
